a780f1962dca7584d16c3c243b50d15a5e424191
tyuan <tyuan@student.42.fr>
2021-11-12 09:39:16 +0100

    adjust parse.h

--- include/parse.h
+++ include/parse.h
@@ -39,27 +39,35 @@ int			expand(char **tabs, char **tab_q, t_exec exec, int type);
 void		var_space_splitting(t_tknlst	*lst_token);
 void		set_argv(t_cml *cml);
 
-/* tool_parse.c */
-void		remove_substr(char **line, int sub_start, int sub_end);
-char		*combine_strings(char **tabs);
-void		str_idx_split(char *l, int *start, int *end, char **tabs);
-int			num_of_vars(t_token *tok);
-int			amount_of_cmls(char **cml_tab);
+/* syntax.c */
+int			check_syntax(char *line, t_exec exec);
+int			unclosed_quot_mrks(char *line, char **p);
 
-/* tool_parse2.c */
+/* heredoc.c */
+char		*available_heredoc_name(void);
+char		*str_variable_expansion(char *line, t_exec exec);
+void		str_expand(char **tabs, t_exec exec);
+void		creat_heredoc(char *delim, int expand, t_exec exec);
+char		*check_heredoc(char *operator, char *line, t_exec exec);
+
+/* tool_cml_redi.c */
 void		init_cml(t_cml *cmls);
+int			amount_of_cmls(char **cml_tab);
+int			num_of_vars(t_token *tok);
 int			if_unquoted_space_in_redi(t_tknlst *lst);
 int			typeof_redi(char *line);
+
+/* tool_heredoc.c */
 void		set_heredoc_path(t_cml *cmls);
 
-/* tool_token.c */
+/* token.c */
 t_token		*new_token(int type, char *nword, char *nquo);
 void		chang_token_value(t_token *tok, int type, char *nword, char *nquo);
 char		*be_not_quoted(char *w);
 char		*set_quoted_bits(char *line);
 char		*quoted_bit_reset(char *line, char c, int *type);
 
-/* tool_parse_split.c */
+/* split.c */
 char		**jump_quotes_ft_split(char const *s, char *quoted, char c);
 int			docp(char **tabs, char const *s, int *ct);
 int			sizec(char const *s, char c);
@@ -71,23 +79,17 @@ int			str_start_with(const char *line, char c);
 int			str_end_with(const char *line, char c);
 char		*str_quote_removal(char *str);
 
-/* tool_tknlst.c */
+/* tool_str2.c */
+void		remove_substr(char **line, int sub_start, int sub_end);
+char		*combine_strings(char **tabs);
+void		str_idx_split(char *l, int *start, int *end, char **tabs);
+
+/* tknlst.c */
 t_tknlst	*tknlstnew(void *tkn);
 void		tknlstiter(t_tknlst *lst, t_exec *op,
 				void (*f)(t_token *, t_exec *));
 void		tknlstadd_back(t_tknlst **alst, t_tknlst *new);
 t_tknlst	*tknlstlast(t_tknlst *lst);
 int			tknlstsize(t_tknlst *lst);
-
-/* tool_syntax.c */
-int			check_syntax(char *line, t_exec exec);
-int			unclosed_quot_mrks(char *line, char **p);
-
-/* heredoc.c */
-char		*available_heredoc_name(void);
-char		*str_variable_expansion(char *line, t_exec exec);
-void		str_expand(char **tabs, t_exec exec);
-void		creat_heredoc(char *delim, int expand, t_exec exec);
-char		*check_heredoc(char *operator, char *line, t_exec exec);
 
 #endif
